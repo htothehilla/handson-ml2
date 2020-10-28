27c74e1e9022cb1b1964af58da34b7b49d60d2a8
hillarykjuma <hillarykimj@gmail.com>
2020-10-28 17:27:57 +0000

    exploring data

--- work_in_progress/chp2.py
+++ work_in_progress/chp2.py
@@ -35,3 +35,98 @@ import matplotlib.pyplot as plt
 housing.hist(bins=50, figsize=(20,15))
 plt.show()
 
+#create a test set
+import numpy as np
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(housing, 0.2)
+len(train_set)
+len(test_set)
+
+#To have a stable train/test split even after updating the dataset,
+#a common solution is to use each instance’s identifier to decide
+#whether or not it should go in the test set
+#(assuming instances have a unique and immutable identifier).
+
+from zlib import crc32
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+
+#spliting dataset
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+#checking if representative, creating continous attribute to a income catgeory
+# five labels for each variable
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+housing["income_cat"].hist()
+
+#stratified sampling based
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+#remove the income_cat attribute so the data is back to its original state:
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+#put training set to aside
+housing = strat_train_set.copy()
+
+#visulaising training set, alpha makes it more visible
+#s = population and c = colour of median house prices
+housing.plot(kind="scatter",x="longitude",y="latitude", alpha=0.1,
+    s=housing["population"]/100,label="population",figsize=(10,7),
+    c="median_house_value",cmap=plt.get_cmap("jet"),colorbar=True,
+)
+plt.legend()
+
+#looking for correlations
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+from pandas.plotting import scatter_matrix
+attributes = ["median_house_value","median_income","total_rooms","housing_median_age"]
+scatter_matrix(housing[attributes],figsize=(12,8))
+
+#zoom in on median income
+housing.plot(kind="scatter",x="median_income",y="median_house_value",alpha=0.1)
+
+#checking out new attributes
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+
+#correlation matrix
+corr_matrix =housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+#
